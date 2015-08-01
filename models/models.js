278c75dc0f86ea61d59cbb62fc20cbd323509b95
franjjardin <akasy@hotmail.com>
2015-08-01 18:43:51 +0200

    Correción errores foreman

--- models/models.js
+++ models/models.js
@@ -1,18 +1,18 @@
-var path = requiere('path');
+var path = require('path');
 
 // Postgres DATABASE_URL = postgres://usr:passwd@host:port/database
 // SQLite DATABASE_url = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name	 = (url[6]||null);
 var user	 = (url[2]||null);
-var pawd	 = (url[3]||null);
+var pwd		 = (url[3]||null);
 var protocol = (url[1]||null);
 var dialect	 = (url[1]||null);
 var port	 = (url[5]||null);
 var host	 = (url[4]||null);
 var storage	 = process.env.DATABASE_STORAGE;
 // Cargar Modelo ORM
-var Sequelize = requiere('sequelize');
+var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd,
@@ -32,14 +32,18 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // exportar defición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function(){
-	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+sequelize.sync().then(function(){
+	// then(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().then(function(count){
 		if (count===0){ // la tabla se inicializa solo si estaá vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'	
+			});
+			Quiz.create({ pregunta: 'Capital de Portugal',
+						  respuesta: 'Lisboa'
+
 			})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,13 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definción de rutas con :quizId
+router.get('/quizes',						quizController.index);
+router.get('/quizes/:quizId(\\d+)',			quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 
 /* GET page Author. */
 router.get('/author', function(req, res) {
